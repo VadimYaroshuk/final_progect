ddcf258219f2cc7d03e8263ef3c91d4d20922cb1
Vadim Yaroshuk <vadim@yaroshuk.spb.ru>
2022-11-25 20:52:41 +0300

    Добавил алгоритм решения задачи

--- Program.cs
+++ Program.cs
@@ -8,3 +8,63 @@
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
 
+void FillStringArray(int n)
+{
+    string[] array1 = new string[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        array1[i] = Console.ReadLine();
+    }
+
+    int c1 = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        int s = array1[i].Length;
+        if (s <= 3)
+        {
+            c1++;
+        }
+    }
+
+    string[] array2 = new string[c1];
+    int c2 = 0;
+    for (int i = 0; i < n; i++)
+    {
+
+
+        if (array1[i].Length <= 3)
+        {
+            array2[c2] = array1[i];
+            c2++;
+        }
+
+    }
+
+    Console.WriteLine();
+
+    if (c2 != 1)
+    {
+        Console.Write("[" + array2[0] + ", ");
+
+        for (int i = 1; i < c2 - 1; i++)
+        {
+            Console.Write(array2[i] + ", ");
+        }
+
+
+        Console.Write(array2[c2 - 1] + "]");
+        Console.WriteLine();
+    }
+
+    else
+    {
+        Console.Write("[" + array2[0] + "]");
+        Console.WriteLine();
+    }
+
+}
+
+
+FillStringArray(5);
